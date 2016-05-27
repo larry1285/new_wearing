0939d0d1f3a5f288d4d7362f2288341d1605b926
larry1285 <larry1285@gmail.com>
2016-05-27 17:06:27 +0800

    create table,insert data

--- app/src/main/java/com/example/larry/new_wearing/Main2Activity.java
+++ app/src/main/java/com/example/larry/new_wearing/Main2Activity.java
@@ -1,9 +1,12 @@
 package com.example.larry.new_wearing;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,13 +14,27 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        SQLiteDatabase sqLiteDatabase =getBaseContext().openOrCreateDatabase("sqlite_test.db",MODE_PRIVATE,null);
+        // We first get a reference to the Intent that resulted in this activity
+        // being started by the Android framework.
         Intent in = getIntent();
+
+        // From the Intent we retrieve the message that was sent from MainActivity
+        // Note the use of the same key, "message", to retrieve the message
         String message = in.getStringExtra("message");
+        Cursor query=sqLiteDatabase.rawQuery("SELECT * from contacts WHERE name='larry' ",null);
+        if(query.moveToFirst()){
+            do{
+                String name=query.getString(0);
+                int phone=query.getInt(1);
+                String email=query.getString(2);
+                Toast.makeText(getBaseContext(),"Name="+name+" Phone="+phone+" email="+email, Toast.LENGTH_LONG).show();
 
-        // Get the reference to the TextView on the ShowMessage UI
-        TextView textMessage = (TextView) findViewById(R.id.textView10);
+            }while(query.moveToNext());
+        }else{
+            Toast.makeText(getBaseContext(),"Error retrieving data",Toast.LENGTH_LONG).show();
+        }
+        sqLiteDatabase.close();
 
-        // set the text of the TextView to display the incoming greeting message
-        textMessage.setText(message);
     }
 }
--- app/src/main/java/com/example/larry/new_wearing/MainActivity.java
+++ app/src/main/java/com/example/larry/new_wearing/MainActivity.java
@@ -2,44 +2,109 @@ package com.example.larry.new_wearing;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    Button my_button;
+    ImageView class_pic;
+    ImageView movie_pic;
+    ImageView shopping_pic;
+    ImageView singlesmixer_pic;
+    ImageView workout_pic;
+    ImageView formal_pic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        my_button=(Button)findViewById(R.id.larry);
-        my_button.setOnClickListener(this);
+
+        SQLiteDatabase sqLiteDatabase =getBaseContext().openOrCreateDatabase("wearing.db",MODE_PRIVATE,null);
+        //drop all the tables(just for now)
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS cloth");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS pants");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS shoes");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS jacket");
+
+        //insert all the datas again(just for now)
+        sqLiteDatabase.execSQL("CREATE TABLE cloth(name TEXT,color TEXT,attribute TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE pants(name TEXT,color TEXT,attribute TEXT,length TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE shoes(name TEXT,color TEXT,attribute TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE jacket(name TEXT,color TEXT,attribute TEXT,temperature TEXT)");
+
+
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('Polo ','紅','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('潮T','白','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('素T','黑','帥')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('潮T','白','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('襯衫','黑','帥')");
+
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('休閒短褲','灰','舒適','短')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('窄庫','卡其色','帥','長')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('運動褲','黑','運動','短')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('球褲','紅','運動','短')");
+
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('球鞋','黑','運動')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('凡布鞋','灰','帥')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('涼鞋','咖啡色','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('皮鞋','黑','正式')");
+
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('薄外套','藍','舒適','凉')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('羽絨外套','灰','舒適','寒')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('運動外套','白','運動','涼')");
+
+
+        sqLiteDatabase.close();
+
+
+
+        class_pic=(ImageView)findViewById(R.id.class_image);
+        class_pic.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()) {
-
-            case R.id.larry:
+            case R.id.class_image:
                 // create a new intent. The first parameter is the Context which is the current Activity.
                 // Hence we use "this". The second parameter is the Activity class that we wish to start.
                 // Hence it is specified as ShowMessage.class
                 Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message", "comfortable");
                 // Add the message as a payload to the Intent. We add data to be carried by the intern using
                 // the putExtra() methods. The data is specified as a key-value pair. The first parameter is
                 // the key, specified as a string, and the second parameter is the value.
-                EditText editFriendName = (EditText) findViewById(R.id.editText);
-                String friendName = editFriendName.getText().toString();
-                in.putExtra("message", friendName +  "!");
-
-                // We start the new activity by calling this method to inform the Android framework to start
-                // the new activity. The parameter is the Intent we just created earlier
                 startActivity(in);
                 break;
+            case R.id.movie_image:
+                Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message","comfortable");
+                startActivity(in);
+            case R.id.shopping_image:
+                Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message","fashion");
+                startActivity(in);
+            case R.id.singlesmixer_image:
+                Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message","fashion");
+                startActivity(in);
+            case R.id.workout_image:
+                Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message","workout");
+                startActivity(in);
+            case R.id.formal_image:
+                Intent in = new Intent(MainActivity.this,Main2Activity.class);
+                in.putExtra("message","formal");
+                startActivity(in);
             default:
                 break;
         }
