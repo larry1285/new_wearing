0f548e1fa88dcbd9d903ddaab5ed02b255a32ba3
larry1285 <larry1285@gmail.com>
2016-05-29 19:49:41 +0800

    choose cloth,pant,shoes,coats

--- app/src/main/java/com/example/larry/new_wearing/Main2Activity.java
+++ app/src/main/java/com/example/larry/new_wearing/Main2Activity.java
@@ -5,36 +5,102 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+    Button temp_button;
+    String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        SQLiteDatabase sqLiteDatabase =getBaseContext().openOrCreateDatabase("sqlite_test.db",MODE_PRIVATE,null);
+        SQLiteDatabase sqLiteDatabase =getBaseContext().openOrCreateDatabase("wearing.db",MODE_PRIVATE,null);
+        temp_button=(Button)findViewById(R.id.button2);
         // We first get a reference to the Intent that resulted in this activity
         // being started by the Android framework.
         Intent in = getIntent();
 
         // From the Intent we retrieve the message that was sent from MainActivity
         // Note the use of the same key, "message", to retrieve the message
-        String message = in.getStringExtra("message");
-        Cursor query=sqLiteDatabase.rawQuery("SELECT * from contacts WHERE name='larry' ",null);
+        message = in.getStringExtra("message");
+        //選衣服
+        Cursor query=sqLiteDatabase.rawQuery("SELECT * from cloth WHERE attribute=?",new String[] { message },null);
         if(query.moveToFirst()){
-            do{
-                String name=query.getString(0);
-                int phone=query.getInt(1);
-                String email=query.getString(2);
-                Toast.makeText(getBaseContext(),"Name="+name+" Phone="+phone+" email="+email, Toast.LENGTH_LONG).show();
 
-            }while(query.moveToNext());
+            String name=query.getString(0);
+            String color=query.getString(1);
+            String attribute=query.getString(2);
+            int ivID = getResources().getIdentifier(name , "drawable", getPackageName());
+            ImageView iv= (ImageView)findViewById(R.id.imageView2);
+            iv.setImageResource(ivID);
+
+        }else{
+            Toast.makeText(getBaseContext(),"Error retrieving data",Toast.LENGTH_LONG).show();
+        }
+
+        //選褲子
+        query=sqLiteDatabase.rawQuery("SELECT * from pants WHERE attribute=?",new String[] { message },null);
+        if(query.moveToFirst()){
+            String name=query.getString(0);
+            String color=query.getString(1);
+            String attribute=query.getString(2);
+            int ivID = getResources().getIdentifier(name , "drawable", getPackageName());
+            ImageView iv= (ImageView)findViewById(R.id.imageView5);
+            iv.setImageResource(ivID);
+
+        }else{
+            Toast.makeText(getBaseContext(),"Error retrieving data",Toast.LENGTH_LONG).show();
+        }
+        //選鞋子
+        query=sqLiteDatabase.rawQuery("SELECT * from shoes WHERE attribute=?",new String[] { message },null);
+        if(query.moveToFirst()){
+            String name=query.getString(0);
+            String color=query.getString(1);
+            String attribute=query.getString(2);
+            int ivID = getResources().getIdentifier(name , "drawable", getPackageName());
+            ImageView iv= (ImageView)findViewById(R.id.imageView4);
+            iv.setImageResource(ivID);
+
+        }else{
+            Toast.makeText(getBaseContext(),"Error retrieving data",Toast.LENGTH_LONG).show();
+        }
+
+        sqLiteDatabase.close();
+
+        temp_button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        //選外套
+        SQLiteDatabase sqLiteDatabase =getBaseContext().openOrCreateDatabase("wearing.db",MODE_PRIVATE,null);
+        Cursor query=sqLiteDatabase.rawQuery("SELECT * from jacket WHERE attribute=?",new String[] { message },null);
+        if(query.moveToFirst()){
+            String name=query.getString(0);
+            String color=query.getString(1);
+            String attribute=query.getString(2);
+            EditText edit_temperature = (EditText) findViewById(R.id.editText);
+            String temperature = edit_temperature.getText().toString();
+            int temp=Integer.parseInt(temperature);
+            int ivID = getResources().getIdentifier(name , "drawable", getPackageName());
+            ImageView iv= (ImageView)findViewById(R.id.imageView6);
+            if(attribute.equals("運動"))
+                iv.setImageResource(R.drawable.sports_coat);
+            else if(temp<23 && temp>13)
+                iv.setImageResource(R.drawable.thin_coat);
+                //iv.setImageResource(ivID);
+            else if(temp<13)
+                iv.setImageResource(R.drawable.feather_coat);
+            else
+                iv.setImageResource(R.drawable.not_recommended);
+
         }else{
             Toast.makeText(getBaseContext(),"Error retrieving data",Toast.LENGTH_LONG).show();
         }
         sqLiteDatabase.close();
-
     }
 }
--- app/src/main/java/com/example/larry/new_wearing/MainActivity.java
+++ app/src/main/java/com/example/larry/new_wearing/MainActivity.java
@@ -40,25 +40,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
         sqLiteDatabase.execSQL("CREATE TABLE jacket(name TEXT,color TEXT,attribute TEXT,temperature TEXT)");
 
 
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('Polo ','紅','舒適')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('潮T','白','舒適')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('素T','黑','帥')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('潮T','白','舒適')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('襯衫','黑','帥')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('poloshirt','紅','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('fashion_t','白','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('plain_t','黑','帥')");
+        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('shirt','黑','運動')");
 
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('休閒短褲','灰','舒適','短')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('窄庫','卡其色','帥','長')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('運動褲','黑','運動','短')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('球褲','紅','運動','短')");
+        sqLiteDatabase.execSQL("INSERT INTO pants VALUES('leisure_pants','灰','舒適','短')");
+        sqLiteDatabase.execSQL("INSERT INTO pants VALUES('narrow_pants','卡其色','帥','長')");
+        sqLiteDatabase.execSQL("INSERT INTO pants VALUES('sports_pants','黑','運動','短')");
+        sqLiteDatabase.execSQL("INSERT INTO pants VALUES('ball_pants','紅','運動','短')");
 
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('球鞋','黑','運動')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('凡布鞋','灰','帥')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('涼鞋','咖啡色','舒適')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('皮鞋','黑','正式')");
+        sqLiteDatabase.execSQL("INSERT INTO shoes VALUES('sneaker','黑','運動')");
+        sqLiteDatabase.execSQL("INSERT INTO shoes VALUES('canvas','灰','帥')");
+        sqLiteDatabase.execSQL("INSERT INTO shoes VALUES('sandals','咖啡色','舒適')");
+        sqLiteDatabase.execSQL("INSERT INTO shoes VALUES('leather_shoes','黑','正式')");
 
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('薄外套','藍','舒適','凉')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('羽絨外套','灰','舒適','寒')");
-        sqLiteDatabase.execSQL("INSERT INTO cloth VALUES('運動外套','白','運動','涼')");
+        sqLiteDatabase.execSQL("INSERT INTO jacket VALUES('thin_coat','藍','舒適','凉')");
+        sqLiteDatabase.execSQL("INSERT INTO jacket VALUES('feather_coat','灰','舒適','寒')");
+        sqLiteDatabase.execSQL("INSERT INTO jacket VALUES('sports_coat','白','運動','涼')");
 
 
         sqLiteDatabase.close();
@@ -66,48 +65,53 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
         class_pic=(ImageView)findViewById(R.id.class_image);
-        class_pic.setOnClickListener(this);
+        movie_pic=(ImageView)findViewById(R.id.movie_image);
+        shopping_pic=(ImageView)findViewById(R.id.shopping_image);
+        singlesmixer_pic=(ImageView)findViewById(R.id.singlesmixer_image);
+        workout_pic=(ImageView)findViewById(R.id.workout_image);
+        formal_pic=(ImageView)findViewById(R.id.formal_image);
 
+        class_pic.setOnClickListener(this);
+        movie_pic.setOnClickListener(this);
+        shopping_pic.setOnClickListener(this);
+        singlesmixer_pic.setOnClickListener(this);
+        workout_pic.setOnClickListener(this);
+        formal_pic.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        Intent in= new Intent(MainActivity.this,Main2Activity.class);
         switch (view.getId()) {
             case R.id.class_image:
                 // create a new intent. The first parameter is the Context which is the current Activity.
                 // Hence we use "this". The second parameter is the Activity class that we wish to start.
                 // Hence it is specified as ShowMessage.class
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message", "comfortable");
+                in.putExtra("message", "舒適");
                 // Add the message as a payload to the Intent. We add data to be carried by the intern using
                 // the putExtra() methods. The data is specified as a key-value pair. The first parameter is
                 // the key, specified as a string, and the second parameter is the value.
-                startActivity(in);
                 break;
             case R.id.movie_image:
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message","comfortable");
-                startActivity(in);
+                in.putExtra("message","舒適");
+                break;
             case R.id.shopping_image:
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message","fashion");
-                startActivity(in);
+                in.putExtra("message","舒適");
+                break;
             case R.id.singlesmixer_image:
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message","fashion");
-                startActivity(in);
+                in.putExtra("message","帥");
+                break;
             case R.id.workout_image:
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message","workout");
-                startActivity(in);
+                in.putExtra("message","運動");
+                break;
             case R.id.formal_image:
-                Intent in = new Intent(MainActivity.this,Main2Activity.class);
-                in.putExtra("message","formal");
-                startActivity(in);
+                in.putExtra("message","正式");
+                break;
             default:
                 break;
         }
+        startActivity(in);
     }
 }
 
